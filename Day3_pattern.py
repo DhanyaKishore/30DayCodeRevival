0e1d87d15714483e05a75bfbe85bb0aec33dd778
Dhanya Kishore <dhanyakishore5@gmail.com>
2025-06-23 21:58:43 +0530

    Add files via upload

---
+++ Day3_pattern.py
@@ -0,0 +1,8 @@
+# Problem: Create a Pattern
+# Platform: HackerRank
+# Date: 2025-06-01
+# Author: Dhanyasree
+
+row=int(input("Enter the number of rows:"))
+for i in range(i,row+1):
+  print("*"*i)
